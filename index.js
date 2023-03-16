ed1f25172dfb5e258e151a7c6386a12bc6587f13
daniel <danielmutisya203@gmail.com>
2023-03-17 00:58:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName= "bob";
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+      
+    };
+    var bestCustomer;
+
+    function setBestCustomer() {
+        bestCustomer = "not bob"; // this reassigns the value of the global variable
+      }
+    
+      function overwriteBestCustomer(){
+        bestCustomer= "maybe bob"
+      }
+const leastFavoriteCustomer= "bob"
+      function changeLeastFavoriteCustomer() {
+        leastFavoriteCustomer = "moses"; 
+      };
+      changeLeastFavoriteCustomer();
